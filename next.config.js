232ef2d99681f0b5b1e2461d68a21325aeb23f3a
jenriquerg <rojasguerrero123@gmail.com>
2025-11-04 16:28:00 -0600

    Fix: Solucionar ambos hosts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  // Solo usar standalone para Docker (no en Vercel)
+  ...(process.env.VERCEL ? {} : { output: 'standalone' }),
   images: {
     remotePatterns: [
       {
